7ce94a61ec37a90ce2cb90eeb8253fde0a00a1fc
Marek Mościchowski <moscich@gmail.com>
2014-02-20 20:19:28 +0100

    Added project

---
+++ Classes/ProductCore.m
@@ -0,0 +1,13 @@
+//
+//  ProductCore.m
+//  ProductCore
+//
+//  Created by Marek Mościchowski on 20/02/14.
+//  Copyright (c) 2014 Marek Mościchowski. All rights reserved.
+//
+
+#import "ProductCore.h"
+
+@implementation ProductCore
+
+@end
---
+++ Project/ProductCore/ProductCoreTests/ProductCoreTests.m
@@ -0,0 +1,34 @@
+//
+//  ProductCoreTests.m
+//  ProductCoreTests
+//
+//  Created by Marek Mościchowski on 20/02/14.
+//  Copyright (c) 2014 Marek Mościchowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProductCoreTests : XCTestCase
+
+@end
+
+@implementation ProductCoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
